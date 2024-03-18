e339967875fc9f24f808acaae728260e73c997ce
Sean Lewertow <seanlewertow@endpoint-10-192-170-241.ufvisitor.ufl.edu>
2024-03-18 18:34:38 -0400

    added project structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-// pages/_app.js (assuming it's directly under the 'pages' folder)
 import '../src/styles/globals.css';
+import Header from '../src/components/header'; // Header component
+import Footer from '../src/components/footer'; // Footer component
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
