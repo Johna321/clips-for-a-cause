276d6560b99951a5ddfc7930c688c00541027741
John Aitken <johna321123@gmail.com>
2024-03-21 17:39:46 -0400

    about page test

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+export default function About() {
+    return (
+        <>
+            <header className="p-6 text-center text-white bg-blue-500">
+                <h1 className="text-4xl font-bold">About Us</h1>
+            </header>
+            <main className="max-w-4xl p-8 mx-auto">
+                <section className="mb-12">
+                  <p>Clips for a Cause is dedicated to breaking down socioeconomic barriers to personal grooming by providing free haircuts to those in need and centralizing fundraising efforts on a dedicated platform.</p>
+                </section>
+            </main>
+        </>
+
+    );
+}
