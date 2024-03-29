52d5a2a819244a28e69d1638c6387930691f0282
John Aitken <johna321123@gmail.com>
2024-03-29 13:01:04 -0400

    styling fixes

--- pages/login.js
+++ pages/login.js
@@ -34,17 +34,25 @@ const Login = () => {
     }
 
     return(
-        <div className="flex flex-col gap-4 bg-card p-5 shadow-shadow shadow-[2px_2px_0_1px_#000] border-text sm:border-[1px] sm:rounded-md border-y-[1px]">
-        <form onSubmit={login} className="flex flex-col gap-4">
-            <div className='text-xl text-center font-header'>Welcome back!</div>
-            {error && <div className='p-2 border bg-errorBackground border-errorBorder rounded-md'>{error}</div>}
-            <input className="p-2 text-black rounded-md bg-background" name="email" type="text" placeholder='Email address' />
-            <input className="p-2 text-black rounded-md bg-background" name="password" type="password" placeholder='Password' />
-            <button className="bg-secondary text-[white] p-2 rounded-md hover:opacity-50">Log in</button>
-        </form>
-        <div className='text-center font-header'>Don't have an account?</div>
-        <Link href="/signup" className="text-center bg-primary text-[white] p-2 rounded-md hover:opacity-50">Sign up for free</Link>
-    </div>
+        <div className="bg-[#F1FAEE] min-h-screen p-10 text-black">
+            <div className="flex flex-col bg-[#A8DADC] gap-4 bg-card p-5 shadow-shadow shadow-[2px_2px_0_1px_#000] border-text sm:border-[1px] sm:rounded-md border-y-[1px]">
+              <header className="bg-[#1D3557] text-white text-center p-6">
+                  <button className="text-4xl font-bold hover:opacity-50" onClick={() => router.push('/')}>Clips for a Cause</button>
+                <p className="text-xl">Making a difference, one haircut at a time.</p>
+              </header>
+                <form onSubmit={login} className="flex flex-col gap-4">
+                    <h1 className='text-xl font-bold text-center font-header'>Welcome back!</h1>
+                    {error && <div className='p-2 border bg-errorBackground border-errorBorder rounded-md'>{error}</div>}
+                    <input className="p-2 text-black rounded-md bg-background" name="email" type="text" placeholder='Email address' />
+                    <input className="p-2 text-black rounded-md bg-background" name="password" type="password" placeholder='Password' />
+                    <button className="text-[white] bg-[#1D3557] p-2 rounded-md hover:opacity-50">Log in</button>
+                </form>
+                <div className="text-center font-header">
+                    <div><i>Don't have an account?</i></div>
+                    <Link href="/signup" className="p-2 text-center bg-primary rounded-md hover:opacity-50">Sign up for free</Link>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- pages/signup.js
+++ pages/signup.js
@@ -50,20 +50,37 @@ const Signup = () => {
 
         }
         catch (err) {
-            console.error("error registering user: ", err);
+            console.log("error registering user: ", err);
+            setError("We're sorry, there was an error signing up. Please try again.");
         }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input name="name" onChange={handleInputChange} type="text" placeholder='Name' className="p-2 text-black rounded-lg bg-background" />
-            <input name="email" onChange={handleInputChange} type="text" placeholder='Email address' className="p-2 text-black rounded-lg bg-background" />
-            <input name="password" onChange={handleInputChange} type="password" placeholder='Password' className="p-2 text-black rounded-lg bg-background" />
-            {success && <div className='p-2 border bg-successBackground border-successBorder rounded-xl'>{success}</div>}
-            <button className="bg-primary text-[white] p-2 rounded-lg hover:opacity-50">Sign up</button>
-        </form>
+        <div className="bg-[#F1FAEE] text-black min-h-screen p-10">
+            <div className="flex flex-col max-w-4xl mx-auto bg-[#A8DADC] gap-4 p-5 shadow-shadow shadow-[2px_2px_0_1px_#000] border-text sm:border-[1px] sm:rounded-md border-y-[1px]">
+              <header className="bg-[#1D3557] text-white text-center p-6">
+                  <button onClick={() => router.push('/')} className="text-4xl font-bold hover:opacity-50">Clips for a Cause</button>
+                <p className="text-xl">Making a difference, one haircut at a time.</p>
+              </header>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <div className='text-xl font-bold text-center font-header'>Welcome!</div>
+                    {error && <div className='p-2 border bg-error border-errorBorder rounded-md'>{error}</div>}
+                    <input onChange={handleInputChange} className="p-2 text-black rounded-md bg-background" name="name" type="text" placeholder='Your name' />
+                    <input onChange={handleInputChange} className="p-2 text-black rounded-md bg-background" name="email" type="text" placeholder='Email address' />
+                    <input onChange={handleInputChange} className="p-2 text-black rounded-md bg-background" name="password" type="password" placeholder='Password' />
+                    <button className="bg-[#1D3557] text-[white] p-2 rounded-md hover:opacity-50">Sign Up</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
 export default Signup;
 
+      //  <form onSubmit={handleSubmit}>
+      //      <input name="name" onChange={handleInputChange} type="text" placeholder='Name' className="p-2 text-black rounded-lg bg-background" />
+      //      <input name="email" onChange={handleInputChange} type="text" placeholder='Email address' className="p-2 text-black rounded-lg bg-background" />
+      //      <input name="password" onChange={handleInputChange} type="password" placeholder='Password' className="p-2 text-black rounded-lg bg-background" />
+      //      {success && <div className='p-2 border bg-successBackground border-successBorder rounded-xl'>{success}</div>}
+      //      <button className="bg-primary text-[white] p-2 rounded-lg hover:opacity-50">Sign up</button>
+      //  </form>
